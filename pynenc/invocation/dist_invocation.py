98a447fc81c9b912dd2bc24424b305c4c68db3dc
Jose Diaz <open.source.route@gmail.com>
2023-07-02 19:27:37 +0200

    Integrating distributed invocation with the runner - set up callbacks for the runner to notify waiting on result

--- pynenc/invocation/dist_invocation.py
+++ pynenc/invocation/dist_invocation.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
+from contextvars import ContextVar
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Generic, Optional
+from typing import Optional, Any
 
+from ..arguments import Arguments
+from .status import InvocationStatus
 from .base_invocation import BaseInvocation
-from ..types import Params, Result, Args
+from ..types import Params, Result
+
+
+# Create a context variable to store current invocation
+context_invocation: ContextVar[DistributedInvocation] = ContextVar("context_invocation")
 
 
 class DistributedInvocation(BaseInvocation[Params, Result]):
@@ -13,10 +20,36 @@ class DistributedInvocation(BaseInvocation[Params, Result]):
         self.app.state_backend.upsert_invocation(self)
 
     @property
+    def status(self) -> "InvocationStatus":
+        """Get the status of the invocation"""
+        return self.app.orchestrator.get_invocation_status(self)
+
+    def run(self) -> None:
+        # Set current invocation
+        token = context_invocation.set(self)
+        try:
+            self.app.orchestrator.set_invocation_status(self, InvocationStatus.RUNNING)
+            result = self.task.func(**self.arguments.kwargs)
+            self.app.state_backend.set_result(self, result)
+            self.app.orchestrator.set_invocation_status(self, InvocationStatus.SUCCESS)
+        except Exception as ex:
+            self.app.state_backend.set_exception(self, ex)
+            self.app.orchestrator.set_invocation_status(self, InvocationStatus.FAILED)
+        finally:
+            # Reset the current invocation to the previous context
+            context_invocation.reset(token)
+
+    @property
     def result(self) -> "Result":
-        raise NotImplementedError("TODO")
-        # self.app.state_backend...
-        # HERE SPECIFIC WAITING BEHAVIOUR!!!
+        current_invocation = context_invocation.get(None)
+        if not self.status.is_final():
+            self.app.orchestrator.waiting_for_result(current_invocation, self)
+
+        while not self.status.is_final():
+            self.app.runner.waiting_for_result(current_invocation, self)
+        if self.status == InvocationStatus.FAILED:
+            raise self.app.state_backend.get_exception(self)
+        return self.app.state_backend.get_result(self)
 
 
 @dataclass(frozen=True)
@@ -25,11 +58,11 @@ class ReusedInvocation(DistributedInvocation):
 
     # Due to single invocation functionality
     # keeps existing invocation + new argument if any change
-    diff_arg: Optional[Args] = None
+    diff_arg: Optional["Arguments"] = None
 
     @classmethod
     def from_existing(
-        cls, invocation: DistributedInvocation, diff_arg: Optional[Args] = None
+        cls, invocation: DistributedInvocation, diff_arg: Optional["Arguments"] = None
     ) -> "ReusedInvocation":
         # Create a new instance with the same fields as the existing invocation, but with the added field
         new_invc = cls(invocation.task, invocation.arguments, diff_arg)
