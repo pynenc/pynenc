e7ff6f103db8dffb278355a16de9aed8a3aaed44
Jose Diaz <open.source.route@gmail.com>
2024-01-01 15:29:47 +0100

    20 Configuration options for specifying subclasses

--- pynenc/conf/config_pynenc.py
+++ pynenc/conf/config_pynenc.py
@@ -7,6 +7,18 @@ class ConfigPynenc(ConfigBase):
 
     Attributes
     ----------
+    app_id : str
+        The id of the application.
+    orchestrator_cls : ConfigField
+        The orchestrator class to use.
+    broker_cls : ConfigField
+        The broker class to use.
+    state_backend_cls : ConfigField
+        The state backend class to use.
+    serializer_cls : ConfigField
+        The serializer class to use.
+    runner_cls : ConfigField
+        The runner class to use.
     dev_mode_force_sync_tasks : ConfigField
         If True, forces tasks to run synchronously, useful for development.
     max_pending_seconds : ConfigField
@@ -16,6 +28,12 @@ class ConfigPynenc(ConfigBase):
         The logging level of the application ('info', 'warning', 'error', etc.).
     """
 
+    app_id = ConfigField("pynenc")
+    orchestrator_cls = ConfigField("MemOrchestrator")
+    broker_cls = ConfigField("MemBroker")
+    state_backend_cls = ConfigField("MemStateBackend")
+    serializer_cls = ConfigField("JsonSerializer")
+    runner_cls = ConfigField("DummyRunner")
     dev_mode_force_sync_tasks = ConfigField(False)
     max_pending_seconds = ConfigField(5.0)
     logging_level = ConfigField("info")
