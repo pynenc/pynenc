78d449c0ed00858046d4de9206ad85fde3979e77
Jose Diaz <open.source.route@gmail.com>
2024-01-20 17:42:50 +0100

    fixing the test

--- tests/unit/runner/test_base_runner.py
+++ tests/unit/runner/test_base_runner.py
@@ -65,12 +65,8 @@ def test_dummy_runner(mock_base_app: "MockPynenc") -> None:
 def test_dummy_runner_waiting_for_result(
     mock_sleep: Mock, mock_base_app: "MockPynenc"
 ) -> None:
-    mock_app = Mock(spec=mock_base_app)
-    mock_app.config_values = None
-    mock_app.config_filepath = None
-    runner = DummyRunner(mock_app)
+    runner = DummyRunner(mock_base_app)
+    runner.conf.invocation_wait_results_sleep_time_sec = -1313
     mock_sleep.reset_mock()
     runner.waiting_for_results(None, None)  # type: ignore
-    mock_sleep.assert_called_once_with(
-        runner.conf.invocation_wait_results_sleep_time_sec
-    )
+    mock_sleep.assert_any_call(-1313)
