7a39cef3c94b74f0c3e53681fad630dac28b23b8
Jose Diaz <code.luis.diaz@proton.me>
2025-03-24 09:58:18 +0100

    fix broken test

--- tests/unit/app/test_mem_app.py
+++ tests/unit/app/test_mem_app.py
@@ -167,6 +167,7 @@ def test_pynenc_getstate_setstate() -> None:
         "config_values": {"runner_cls": "ThreadRunner"},
         "config_filepath": None,
         "reporting": None,
+        "tasks": {},
     }
     assert state == expected_state
 
